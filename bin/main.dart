589f547e825c8097226d12785175cc4ea143a197
McLarenCollege <hello@mclarencollege.in>
2019-10-24 10:44:16 +0530

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,171 @@
+main() {}
+
+/// Sample Output
+// Think of an animal
+// Is it a duck?
+// No
+// Oops - looks like I need to improve.
+// What is the animal?
+// A monkey
+// What question would distinguish between a duck and A monkey?
+// Can it fly?
+// For A monkey, is the answer yes or no?
+// No
+// Thanks for helping me to improve!
+//
+// Want to play again?
+// Yes
+// Think of an animal
+// Can it fly?
+// Yes
+// Is it a duck?
+// No
+// Oops - looks like I need to improve.
+// What is the animal?
+// Parrot
+// What question would distinguish between a duck and Parrot?
+// Can it swim?
+// For Parrot, is the answer yes or no?
+// No
+// Thanks for helping me to improve!
+//
+// Want to play again?
+// Yes
+// Think of an animal
+// Can it fly?
+// No
+// Is it A monkey?
+// Yes
+// Yay!  I got it!
+//
+// Want to play again?
+// Yes
+// Think of an animal
+// Can it fly?
+// No
+// Is it A monkey?
+// No
+// Oops - looks like I need to improve.
+// What is the animal?
+// Dog
+// What question would distinguish between A monkey and Dog?
+// Can it climb trees?
+// For Dog, is the answer yes or no?
+// no
+// Thanks for helping me to improve!
+//
+// Want to play again?
+// yes
+// Think of an animal
+// Can it fly?
+// No
+// Can it climb trees?
+// No
+// Is it Dog?
+// No
+// Oops - looks like I need to improve.
+// What is the animal?
+// a cow
+// What question would distinguish between Dog and a cow?
+// Is it taller than 1 meter?
+// For a cow, is the answer yes or no?
+// yes
+// Thanks for helping me to improve!
+//
+// Want to play again?
+// yes
+// Think of an animal
+// Can it fly?
+// No
+// Can it climb trees?
+// Yes
+// Is it A monkey?
+// No
+// Oops - looks like I need to improve.
+// What is the animal?
+// a cat
+// What question would distinguish between A monkey and a cat?
+// Is it a pet?
+// For a cat, is the answer yes or no?
+// yes
+// Thanks for helping me to improve!
+//
+// Want to play again?
+// no
+
+
+//In Class Run Through
+// Think of animal
+// Is it a duck?
+/// no
+// improve me
+// What is the animal
+/// Tiger
+// Distinguishing question b/w tiger and duck
+/// Can it Fly?
+// For Tiger is the answer yes/no?
+/// No
+
+// Think of an Animal
+// Can it fly?
+/// yes
+// Is it a duck?
+/// no
+// improve me
+// What is the animal
+/// Eagle
+// improving question
+/// Is it a predatory animal
+// for eagle is it yes or no
+/// yes
+//
+// Think of an Animal
+// Can it fly?
+/// no
+// Is it a tiger?
+/// no
+// Which animal?
+/// Elephant
+// distinguish b/w elep and tiger
+/// Is it herbivorous?
+// is the answer for elephant yes/no
+/// yes
+
+// Think of an Animal
+// Can it fly?
+/// no
+// Is it herbivorous?
+/// yes
+// Elephant is it?
+/// no
+// which one it is imrpve me
+/// Cow
+// distinguish b/w elephant and cow
+/// does it have trunk
+// for cow is it yes or no
+/// no
+
+// Think of animal
+//  Can it fly?
+/// yes
+// Is it a predator?
+/// yes
+// is it a eagle?
+/// no
+// improve me
+/// bat
+// distinguish question bat and eagle
+/// Is it nocturnal
+// yes or no for bat
+/// yes
+
+// Think of animal
+//  Can it fly?
+/// yes
+// Is it a predator?
+/// yes
+// is it nocturnal?
+/// no
+// is it eagle
+/// yes
+// YAYYY
---
+++ test/Animal_Guess_Game_test.dart
@@ -0,0 +1,8 @@
+import 'package:Animal_Guess_Game/Animal_Guess_Game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
